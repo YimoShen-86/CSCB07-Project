80e66e221805b340dda261be40379a38b93fbe87
beier <beier.mu@mail.utoronto.ca>
2022-12-03 13:15:06 -0500

    course taken v2

--- app/src/main/java/com/example/loginpage/Coursestaken.java
+++ app/src/main/java/com/example/loginpage/Coursestaken.java
@@ -2,13 +2,51 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Coursestaken extends AppCompatActivity {
+    private Button btnInsert, btnView;
+    private EditText course;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference().child("Taken Courses");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coursestaken);
+
+        btnInsert = findViewById(R.id.btninsert);
+        btnView = findViewById(R.id.btnview);
+        course = findViewById(R.id.coursename);
+
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Coursestaken.this , coursecart.class));
+
+            }
+        });
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String coursename = course.getText().toString();
+                HashMap<String, String> userMap = new HashMap<>();
+
+                userMap.put("coursecode", coursename);
+
+                root.push().setValue(userMap);
+            }
+        });
     }
 }
