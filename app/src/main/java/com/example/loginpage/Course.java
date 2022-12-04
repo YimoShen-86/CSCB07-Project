e4e7ead9417b3f1e574e778a70603636badaf33d
JiachenZhu123 <realtmxk@gmail.com>
2022-12-04 02:47:39 -0500

    Progress in student
    Working on student part
    add some methods in model
    to be done tomorrow

--- app/src/main/java/com/example/loginpage/Course.java
+++ app/src/main/java/com/example/loginpage/Course.java
@@ -3,7 +3,9 @@ package com.example.loginpage;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 public class Course implements Serializable {
     public String code; // course code (id)
@@ -34,5 +36,38 @@ public class Course implements Serializable {
         if (o == null || getClass() != o.getClass()) return false;
         Course course = (Course) o;
         return (this.code).equals(course.code);
+    }
+
+    public static List<Course> getCoursePath(HashMap<String, Course> courses, String code) {
+        if (!courses.containsKey(code)) {
+            return null;
+        }
+
+        List<Course> path = new ArrayList<Course>();
+        Stack<String> stk = new Stack<String>();
+        Stack<Course> neededCourse = new Stack<Course>();
+
+        stk.push(code);
+        String courseCode;
+
+        while (!stk.empty()) {
+            String cur = stk.pop();
+            Course curCourse = courses.get(cur);
+            if(neededCourse.search(curCourse) > -1){
+                continue;
+            }else{
+                neededCourse.push(curCourse);
+            }
+
+            for (String next : curCourse.pre) {
+                if(stk.search(next) > -1) continue;
+                else stk.push(next);
+            }
+        }
+        while(!neededCourse.empty()){
+            Course curCourse = neededCourse.pop();
+            if(!path.contains(curCourse)) path.add(curCourse);
+        }
+        return path;
     }
 }
--- app/src/main/java/com/example/loginpage/coursemodel.java
+++
@@ -1,9 +0,0 @@
-package com.example.loginpage;
-
-public class coursemodel {
-    String ccode;
-
-    public String getCcode() {
-        return ccode;
-    }
-}
